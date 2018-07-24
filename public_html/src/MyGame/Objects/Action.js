7b4d4363a4549f65e04c4a83627910a6945f6e58
MashiloYan <34890088+MashiloYan@users.noreply.github.com>
2018-07-24 16:40:06 +0800

    item

--- public_html/src/MyGame/Objects/Action.js
+++ public_html/src/MyGame/Objects/Action.js
@@ -4,4 +4,17 @@
  * and open the template in the editor.
  */
 
+"use strict";  // Operate in Strict mode such that variables must be declared before used!
+
+this.AllItem = ["1.png", "2.png", "3.png"];
+
+function Action() {
+    this.Id = -1;
+    this.Info = "";
+    this.Health = 0;
+    this.mHealth = 0;
+    this.Hunger = 0;
+    this.mHunger = 0;
+    this.getItem = [];
+}
 
--- public_html/src/MyGame/Objects/Item.js
+++ public_html/src/MyGame/Objects/Item.js
@@ -3,5 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+"use strict";  // Operate in Strict mode such that variables must be declared before used!
 
+this.AllItem = ["1.png", "2.png", "3.png"];
 
+function Item() {
+    this.Id = -1;
+    this.Info = "";
+    this.Health = 0;
+    this.mHealth = 0;
+    this.Hunger = 0;
+    this.mHunger = 0;
+    this.render = null;
+    this.eventType = -1; 
+    
+}
+
+Item.setRender = function (r){
+    this.render = r;
+}
