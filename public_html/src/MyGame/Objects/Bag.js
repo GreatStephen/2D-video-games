a9b4a0293ba424f0d68d16a758cda8e89478cb9c
MashiloYan <34890088+MashiloYan@users.noreply.github.com>
2018-07-24 20:48:31 +0800

    hunger decrease with time

--- public_html/src/MyGame/Objects/Bag.js
+++ public_html/src/MyGame/Objects/Bag.js
@@ -11,8 +11,9 @@ var delta = 4.6;
 var deltaMove = 0;
 var lastMove=0;
 
-function Bag(myTexture){
+function Bag(myTexture,cursorTexture){
     console.log("hi");
+    this.current = 0;
     this.itemNum = 0;
     this.itemSet = [];   //items that in the bag,they are in order
     
@@ -22,8 +23,16 @@ function Bag(myTexture){
     this.bag.setColor([0,0,0,0]);
     this.bag.getXform().setSize(30,60);
     this.bag.getXform().setPosition(82,50);
+
+    this.cursor = new TextureRenderable(cursorTexture);;
+    this.cursor.setColor([0,0,0,0]);
+    this.cursor.getXform().setSize(5.7,5.7);
+    this.cursor.getXform().setPosition(x,y);
     
+   // for(var i=0;i<20;i++){
+
     for(var i=0;i<2;i++){
+
         this.AddItem(0);
         //this.AddItem(1);
     }
@@ -44,21 +53,11 @@ Bag.prototype.AddItem = function(id){
 Bag.prototype.Move = function(deltaX){
     var temp = this.bag.getXform().mPosition;
     this.bag.getXform().setPosition(temp[0]+deltaX,temp[1]);
+    temp = this.cursor.getXform().mPosition;
+    this.cursor.getXform().setPosition(temp[0]+deltaX,temp[1]);
+    
     deltaMove += deltaX;
-}
-/*Bag.prototype.AddItem1 = function(id){
-    this.itemSet[this.itemNum]=new Item(id);
-    this.itemSet[this.itemNum].renderable.getXform().setSize(3.5,3.5);
-    this.itemSet[this.itemNum].renderable.getXform().setPosition(72.2,48.1);
-    this.itemNum++;
 };
-
-Bag.prototype.AddItem2 = function(id){
-    this.itemSet[this.itemNum]=new Item(id);
-    this.itemSet[this.itemNum].renderable.getXform().setSize(3.5,3.5);
-    this.itemSet[this.itemNum].renderable.getXform().setPosition(76.8,43.5);
-    this.itemNum++;
-};*/
 
 Bag.prototype.Draw = function(aCamera){
     //console.log(move);
@@ -79,5 +78,34 @@ Bag.prototype.Draw = function(aCamera){
             this.itemSet[i].renderable.draw(aCamera);
         }
     }
-    
+    this.cursor.draw(aCamera);
 };
+
+Bag.prototype.update = function(){
+    var temp = this.cursor.getXform().mPosition;
+    
+    if(gEngine.Input.isKeyClicked(gEngine.Input.keys.Left)){
+        if(this.current%5!=0){
+            this.current--;
+            temp[0] -= delta;
+        }
+    }
+    if(gEngine.Input.isKeyClicked(gEngine.Input.keys.Right)){
+        if(this.current%5!=4){
+            this.current++;
+            temp[0] += delta;
+        }
+    }
+    if(gEngine.Input.isKeyClicked(gEngine.Input.keys.Up)){
+        if(this.current>4){
+            this.current -= 5;
+            temp[1] += delta;
+        }
+    }
+    if(gEngine.Input.isKeyClicked(gEngine.Input.keys.Down)){
+        if(this.current<15){
+            this.current += 5;
+            temp[1] -= delta;
+        }
+    }    
+}
