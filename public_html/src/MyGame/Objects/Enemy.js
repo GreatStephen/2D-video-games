54a8cd323f5985946c2f5254cbd0e6518f8328b9
MashiloYan <34890088+MashiloYan@users.noreply.github.com>
2018-07-29 23:39:24 +0800

    value

--- public_html/src/MyGame/Objects/Enemy.js
+++ public_html/src/MyGame/Objects/Enemy.js
@@ -5,18 +5,18 @@
  */
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
 
-var AllEnemy = [{Id:0, msg:"", mHealth:"30", atk:13, def:2, numItem:1, dropItemId:0, money:20},//mushroom
-    {Id:1, msg:"", mHealth:"40", atk:13, def:3, numItem:1, dropItemId:1, money:30},//eagle
-    {Id:2, msg:"", mHealth:"50", atk:15, def:5, numItem:2, dropItemId:3, money:30},//knight
+var AllEnemy = [{Id:0, msg:"", mHealth:"30", atk:15, def:0, numItem:1, dropItemId:0, money:20},//mushroom
+    {Id:1, msg:"", mHealth:"40", atk:15, def:0, numItem:1, dropItemId:1, money:30},//eagle
+    {Id:2, msg:"", mHealth:"50", atk:18, def:2, numItem:2, dropItemId:3, money:50},//knight
     {Id:3, msg:"", mHealth:"50", atk:15, def:0, numItem:0, dropItemId:0, money:10},
-    {Id:4, msg:"", mHealth:"60", atk:15, def:3, numItem:0, dropItemId:0, money:20}, //dog
-    {Id:5, msg:"", mHealth:"60", atk:15, def:3, numItem:0, dropItemId:0, money:20},// wolf
-    {Id:6, msg:"", mHealth:"100", atk:19, def:3, numItem:0, dropItemId:0, money:20}, // assassin
-    {Id:7, msg:"", mHealth:"100", atk:20, def:5, numItem:0, dropItemId:0, money:0}, // king
-    {Id:8, msg:"", mHealth:"100", atk:25, def:5, numItem:0, dropItemId:0, money:0}, // duke
-    {Id:9, msg:"", mHealth:"100", atk:20, def:8, numItem:0, dropItemId:0, money:0}, // princess
-    {Id:10, msg:"", mHealth:"100", atk:17, def:5, numItem:1, dropItemId:3, money:30}, // soldier
-    {Id:11, msg:"", mHealth:"100", atk:17, def:5, numItem:2, dropItemId:3, money:30}, // captain
+    {Id:4, msg:"", mHealth:"50", atk:20, def:0, numItem:0, dropItemId:0, money:30}, //dog
+    {Id:5, msg:"", mHealth:"50", atk:20, def:0, numItem:0, dropItemId:0, money:30},// wolf
+    {Id:6, msg:"", mHealth:"60", atk:23, def:0, numItem:0, dropItemId:0, money:30}, // assassin
+    {Id:7, msg:"", mHealth:"100", atk:24, def:5, numItem:0, dropItemId:0, money:0}, // king
+    {Id:8, msg:"", mHealth:"100", atk:24, def:5, numItem:0, dropItemId:0, money:0}, // duke
+    {Id:9, msg:"", mHealth:"100", atk:24, def:5, numItem:0, dropItemId:0, money:0}, // princess
+    {Id:10, msg:"", mHealth:"70", atk:20, def:5, numItem:1, dropItemId:3, money:40}, // soldier
+    {Id:11, msg:"", mHealth:"80", atk:20, def:7, numItem:2, dropItemId:3, money:40}, // captain
     ]
 
 function Enemy(id) {
